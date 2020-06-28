7f417f7d3a70005ad38549f9d34c0f4c9c1085f2
vulpix <497350746@22.com>
2020-06-28 12:35:16 +0800

    fixed:修复了一个bug

--- assets/media/scripts/button.js
+++ assets/media/scripts/button.js
@@ -2,16 +2,20 @@ const button = document.getElementById('button');
 let currentState = 'hide';
 const leftContent = document.querySelector('.left-content');
 const rigtContent = document.querySelector('.main-content');
+const main = document.querySelector('.main');
 button.addEventListener('click', () => {
   if (currentState === 'hide') {
     leftContent.style.left = '0px';
-    console.log(rigtContent);
+    main.style.overflow="hidden";
+    rigtContent.style.marginLeft="300px";
+    // console.log(rigtContent);
     // rigtContent.style.position = 'absoulte';
     // rigtContent.style.left = '300px';
     currentState = 'show';
   } else {
     leftContent.style.left = '-300px';
-    // rigtContent.style.paddingLeft = '10px';
+    main.style.overflow="hidden";
+    rigtContent.style.marginLeft="0px";
     currentState = 'hide';
   }
 });
